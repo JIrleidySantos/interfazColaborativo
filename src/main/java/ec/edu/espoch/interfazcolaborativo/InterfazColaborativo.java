270287c476e56868aa67d20022afe261289f2b93
JIrleidySantos <jirleidysantosm@gmail.com>
2025-01-16 10:09:31 -0500

    Crear un repositorio

---
+++ src/main/java/ec/edu/espoch/interfazcolaborativo/InterfazColaborativo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.interfazcolaborativo;
+
+/**
+ *
+ * @author mundo
+ */
+public class InterfazColaborativo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
