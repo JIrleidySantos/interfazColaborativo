ed5cad39df05ea00c422f78bc2d0e4e7f4726b0b
JIrleidySantos <jirleidysantosm@gmail.com>
2025-01-16 10:11:31 -0500

    Mi rama

--- src/main/java/ec/edu/espoch/interfazcolaborativo/InterfazColaborativo.java
+++ src/main/java/ec/edu/espoch/interfazcolaborativo/InterfazColaborativo.java
@@ -11,6 +11,6 @@ package ec.edu.espoch.interfazcolaborativo;
 public class InterfazColaborativo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello d!");
     }
 }
